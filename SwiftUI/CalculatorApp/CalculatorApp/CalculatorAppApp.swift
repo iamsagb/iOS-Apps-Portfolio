ff9fb19d295684faf8fdd9c88025c0a3eb2da911
Sagar Bhosale <bsagar242@icloud.com>
2022-04-19 20:07:58 +0530

    First commit

---
+++ SwiftUI/CalculatorApp/CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Sagar Bhosale on 13/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
